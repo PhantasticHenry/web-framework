7bfa804a8d7cfa3a42274ae878baa792376687ee
github username <github email address>
2021-06-28 15:41:35 -0700

    refactored with composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,11 +6,7 @@ interface UserProps {
 	age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-	events: { [key: string]: Callback[] } = {};
-
 	constructor(private data: UserProps) {}
 
 	get(propName: string): number | string {
@@ -19,24 +15,7 @@ export class User {
 
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
-	}
-
-	on(eventName: string, callback: Callback): void {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-
-		handlers.forEach((callback) => {
-			callback();
-		});
-	}
+	}¡
 
 	fetch(): void {
 		axios
