65aeb0004dbac58f5d9c6ec5e791c795c7601aa8
github username <github email address>
2021-06-28 12:26:18 -0700

    used axios to test request

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20 });
+// axios.post('http://localhost:3000/users', {
+// 	name: 'me',
+// 	age: 22,
+// });
 
-user.on('change', () => {
-	console.log('Change #1');
-});
-user.on('change', () => {
-	console.log('Change #2');
-});
-user.on('click', () => {
-	console.log('Clicked');
-});
-user.on('save', () => {
-	console.log('Save was triggered');
-});
-
-user.trigger('click');
-user.trigger('change');
-user.trigger('save');
-
-console.log(user);
+axios.get('http://localhost:3000/users/1');
