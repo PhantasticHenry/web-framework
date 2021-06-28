1d2ae6ee5069c9b25149b335d8375caf9a45e5a9
github username <github email address>
2021-06-28 16:39:56 -0700

    nsted objects with composition

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2023,7 +2023,10 @@ var user = new User_1.User({
   name: 'new record',
   age: 0
 });
-user.save();
+user.events.on('change', function () {
+  console.log('change!!!!!');
+});
+user.events.trigger('change');
 },{"./models/User":"src/models/User.ts"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
