65e3d39552a0e7c6574bb6555c099db9d88a3bbb
github username <github email address>
2021-07-14 17:18:19 -0400

    Binded Event Handlers

--- src/views/UserForms.ts
+++ src/views/UserForms.ts
@@ -4,11 +4,16 @@ export class UserForm {
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:button': this.onButtonClick,
+			'mouseover:h1': this.onHeaderHover,
 		};
 	}
 
+	onHeaderHover(): void {
+		console.log('H1 was hovered over');
+	}
+
 	onButtonClick(): void {
-		console.log('Hi there');
+		console.log('Button clicked');
 	}
 
 	template(): string {
@@ -23,9 +28,22 @@ export class UserForm {
         `;
 	}
 
+	bindEvents(fragment: DocumentFragment): void {
+		const eventsMap = this.eventsMap();
+		for (let eventKey in eventsMap) {
+			const [eventName, selector] = eventKey.split(':');
+
+			fragment.querySelectorAll(selector).forEach((element) => {
+				element.addEventListener(eventName, eventsMap[eventKey]);
+			});
+		}
+	}
+
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
+
+		this.bindEvents(templateElement.content);
 
 		this.parent.append(templateElement.content);
 	}
