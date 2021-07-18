e9d8f4dd04a8f2352563ee6059b0cfa6970f5cf0
github username <github email address>
2021-07-18 07:20:07 -0400

    added strict null checks thourgh tsconfig

--- src/views/UserForms.ts
+++ src/views/UserForms.ts
@@ -1,13 +1,31 @@
 import { User } from '../models/User';
 
 export class UserForm {
-	constructor(public parent: Element, public model: User) {}
+	constructor(public parent: Element, public model: User) {
+		this.bindModel();
+	}
+
+	bindModel(): void {
+		this.model.on('change', () => {
+			this.render();
+		});
+	}
 
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:.set-age': this.onSetAgeClick,
+			'click:.set-name': this.onSetNameClick,
 		};
 	}
+
+	onSetNameClick = () => {
+		const input = this.parent.querySelector('input');
+
+		if (input) {
+			const name = input.value;
+			this.model.set({ name });
+		}
+	};
 
 	onSetAgeClick = (): void => {
 		this.model.setRandomAge();
@@ -22,7 +40,7 @@ export class UserForm {
             <div>User name: ${this.model.get('name')}</div>
             <div>User age: ${this.model.get('age')}</div>
             <input>
-            <button>Click Me</button>
+            <button class="set-name">Update Name</button>
             <button class="set-age">Set Random Age</button>
         </div>
         `;
@@ -40,6 +58,7 @@ export class UserForm {
 	}
 
 	render(): void {
+		this.parent.innerHTML = '';
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
 
