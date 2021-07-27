f42c287bb063ae8f114a7be875dca06d43a4a9e4
github username <github email address>
2021-07-26 23:53:31 -0700

    Extended with generic constraints

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -2406,7 +2406,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59287" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51861" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
